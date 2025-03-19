cd3390873a3e9a4d65668aa053bdfbdc93706323
Bjorn-Eu <bjorn.eurenius@gmail.com>
2025-03-19 14:56:23 +0000

    .

--- day17.py
+++ day17.py
@@ -27,6 +27,7 @@ class Register:
         self.b = self.combo_map(operand)%8
         
     def jnz(self,operand):
+        #done externally
         pass
 
     def bxc(self,operand):
@@ -44,7 +45,43 @@ class Register:
     def print_registry(self):
         print('\nA:',self.a,'B:',self.b,'C:',self.c)
 
+    def print_registry_octal(self):
+        print('\nA:',oct(self.a),'B:',oct(self.b),'C:',oct(self.c))
 
+def run_program(instructions,a=0,b=0,c=0,print=False):
+    reg = Register(a,0,0)
+    output = []
+    length = len(instructions)
+    pointer = 0
+    while pointer<length:
+        instruction = instructions[pointer]
+        operand = instructions[pointer+1]
+        pointer +=2
+        if instruction == 0:
+            reg.adv(operand)
+        elif instruction == 1:
+            reg.bxl(operand)
+        elif instruction == 2:
+            reg.bst(operand)
+        elif instruction == 3:
+            #jnz(operand)
+            if reg.a != 0:
+                pointer = operand
+        elif instruction == 4:
+            reg.bxc(operand)
+        elif instruction == 5:
+            output.append(reg.combo_map(operand)%8)
+            if print:
+                reg.out(operand)
+            
+        elif instruction == 6:
+            reg.bdv(operand)
+        elif instruction == 7:
+            reg.cdv(operand)
+
+        #reg.print_registry_octal()
+
+    return output
 
 f = open('data/day17.txt')
 data = (f.read())
@@ -57,39 +94,29 @@ reg =Register(a,b,c)
 
 
 print('Running program, the output is:')
-length = len(instructions)
-pointer = 0
-while pointer<length:
-    instruction = instructions[pointer]
-    operand = instructions[pointer+1]
-    if instruction == 0:
-        reg.adv(operand)
-    elif instruction == 1:
-        reg.bxl(operand)
-    elif instruction == 2:
-        reg.bst(operand)
-    elif instruction == 3:
-        #jnz(operand)
-        if reg.a != 0:
-            pointer = -2
-    elif instruction == 4:
-        reg.bxc(operand)
-    elif instruction == 5:
-        reg.out(operand)
-    elif instruction == 6:
-        reg.bdv(operand)
-    elif instruction == 7:
-        reg.cdv(operand)
+run_program(instructions,a=a,b=b,c=c,print=True)
 
-    pointer +=2
-    #reg.print_registry()
+index = len(instructions)-1
+found_values = {}
+base_sums = set()
+base_sums.add(0)
 
-#target = '0,3,5,4,3,0'
-#target = '2,4,1,3,7,5,0,3,4,1,1,5,5,5,3,0,'
-k=117440
-print('Testing stuff')
-reg = Register(k,0,0)
-reg.adv(3)
-reg.out(4)
-reg.out(4)
-reg.print_registry()
+all_solutions = set()
+while index >= 0:
+    new_base_sums = set()
+    for base_sum in base_sums:
+        for i in range(1,8):
+            k = i*(8**index)+base_sum
+            out = run_program(instructions,a=k)
+            if out[index] == instructions[index]:
+                
+                new_base_sums.add(k)
+                if (index == 0):
+                    all_solutions.add(k)
+    index -= 1
+    base_sums = new_base_sums
+
+
+print('')
+print('All solutions found are:',all_solutions)
+print('Minimum solution is:',min(all_solutions))
